6dba2b53356501325f7d220f32eb6921666f42b0
Arno Siegenthaler <arno.siegenthaler44@gmail.com>
2021-03-09 23:08:16 +0100

    initial commit 2

---
+++ go.mod
@@ -0,0 +1,18 @@
+module test
+
+go 1.16
+
+require (
+	github.com/DataDog/zstd v1.4.8 // indirect
+	github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
+	github.com/go-echarts/go-echarts/v2 v2.2.3
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/showwin/speedtest-go v1.1.1
+	github.com/wcharczuk/go-chart v2.0.1+incompatible // indirect
+	github.com/wcharczuk/go-chart/v2 v2.1.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210305230114-8fe3ee5dd75b // indirect
+)
---
+++ main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/components"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/showwin/speedtest-go/speedtest"
+)
+
+var results = make(Results, 7)
+
+func main() {
+
+	if _, err := os.Stat("results.json"); err != nil {
+		results[0].Ping = ""
+		results[0].Download = 0.0
+		results[0].Upload = 0.0
+		results[0].MeasureTime = ""
+		file, _ := json.MarshalIndent(results, "", " ")
+		_ = ioutil.WriteFile("results.json", file, 0777)
+	}
+
+	jsonFile, _ := os.Open("results.json")
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &results)
+
+	ping, download, upload, measureTime := testInternetSpeed()
+
+	if results[0].MeasureTime == "" {
+		fmt.Println("if")
+		results[0].Ping = ping
+		results[0].Download = download
+		results[0].Upload = upload
+		results[0].MeasureTime = measureTime
+	} else if results[1].MeasureTime == "" {
+		fmt.Println("if")
+		results[1].Ping = ping
+		results[1].Download = download
+		results[1].Upload = upload
+		results[1].MeasureTime = measureTime
+	} else if results[2].MeasureTime == "" {
+		fmt.Println("if")
+		results[2].Ping = ping
+		results[2].Download = download
+		results[2].Upload = upload
+		results[2].MeasureTime = measureTime
+	} else if results[3].MeasureTime == "" {
+		fmt.Println("if")
+		results[3].Ping = ping
+		results[3].Download = download
+		results[3].Upload = upload
+		results[3].MeasureTime = measureTime
+	} else if results[4].MeasureTime == "" {
+		fmt.Println("if")
+		results[4].Ping = ping
+		results[4].Download = download
+		results[4].Upload = upload
+		results[4].MeasureTime = measureTime
+	} else if results[5].MeasureTime == "" {
+		fmt.Println("if")
+		results[5].Ping = ping
+		results[5].Download = download
+		results[5].Upload = upload
+		results[5].MeasureTime = measureTime
+	} else if results[6].MeasureTime == "" {
+		fmt.Println("if")
+		results[6].Ping = ping
+		results[6].Download = download
+		results[6].Upload = upload
+		results[6].MeasureTime = measureTime
+	} else {
+		fmt.Println("else")
+		temp1 := results[1]
+		temp2 := results[2]
+		temp3 := results[3]
+		temp4 := results[4]
+		temp5 := results[5]
+		temp6 := results[6]
+
+		results[5] = temp6
+		results[4] = temp5
+		results[3] = temp4
+		results[2] = temp3
+		results[1] = temp2
+		results[0] = temp1
+
+		results[6].Ping = ping
+		results[6].Download = download
+		results[6].Upload = upload
+		results[6].MeasureTime = measureTime
+	}
+
+	file, _ := json.MarshalIndent(results, "", " ")
+	_ = ioutil.WriteFile("results.json", file, 0777)
+
+	page := components.NewPage()
+	page.AddCharts(
+		lineShowLabel(),
+	)
+	f, err := os.Create("index.html")
+	if err != nil {
+		panic(err)
+	}
+	page.Render(io.MultiWriter(f))
+
+}
+
+func lineShowLabel() *charts.Line {
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Measure the Internet Speed of Sh*tcom every 30 minutes",
+			Subtitle: "A litte dirty and buggy tool, that measures the speed of your internet every 30 minutes",
+			Link:     "https://github.com/go-echarts/go-echarts",
+		}),
+	)
+	fruits := []string{
+		time.Now().Add(time.Minute * 0).Format("15:04:05"),
+		time.Now().Add(time.Minute * 20).Format("15:04:05"),
+		time.Now().Add(time.Minute * 40).Format("15:04:05"),
+		time.Now().Add(time.Minute * 60).Format("15:04:05"),
+		time.Now().Add(time.Minute * 80).Format("15:04:05"),
+		time.Now().Add(time.Minute * 100).Format("15:04:05"),
+		time.Now().Add(time.Minute * 120).Format("15:04:05")}
+	testt := make([]opts.LineData, 0)
+
+	testt = append(testt, opts.LineData{Value: results[0].Download})
+	testt = append(testt, opts.LineData{Value: results[1].Download})
+	testt = append(testt, opts.LineData{Value: results[2].Download})
+	testt = append(testt, opts.LineData{Value: results[3].Download})
+	testt = append(testt, opts.LineData{Value: results[4].Download})
+	testt = append(testt, opts.LineData{Value: results[5].Download})
+	testt = append(testt, opts.LineData{Value: results[6].Download})
+
+	line.SetXAxis(fruits).
+		AddSeries("Category A", testt).
+		SetSeriesOptions(
+			charts.WithLabelOpts(opts.Label{
+				Show: true,
+			}),
+		)
+	return line
+}
+
+func testInternetSpeed() (ping string, download float64, upload float64, measureTime string) {
+	user, err := speedtest.FetchUserInfo()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	serverList, err := speedtest.FetchServerList(user)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	targets, err := serverList.FindServer([]int{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, s := range targets {
+		s.PingTest()
+		s.DownloadTest(true)
+		s.UploadTest(true)
+		ping = s.Latency.String()
+		download = math.Round(s.DLSpeed * 100 / 100)
+		upload = math.Round(s.ULSpeed * 100 / 100)
+	}
+	measureTime = time.Now().Format("15:04:05")
+	return ping, download, upload, measureTime
+}
+
+type Results []struct {
+	MeasureTime string  `json:"MeasureTime"`
+	Ping        string  `json:"Ping"`
+	Download    float64 `json:"Download"`
+	Upload      float64 `json:"Upload"`
+}
